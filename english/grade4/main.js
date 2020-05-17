49555430bcb349df7d737dff0f1a77c103435193
Rethna Ganesh <rethna2@gmail.com>
2020-05-17 08:32:37 +0530

    Create main.js

---
+++ english/grade4/main.js
@@ -0,0 +1,222 @@
+const list = [
+          {
+            label: 'Shin Chan',
+            type: "passage",
+            data: {
+              title: `Shin Chan`,
+              img: 'https://s3.ap-south-1.amazonaws.com/pschool.in/assets/english/chinshan.webp',
+              text: `My favourite cartoon is Shin chan. It is the story about a five-year-old boy who is full of mischief. He is always looking for adventure. He loves life and he is very optimistic about it. He is very fond of Action Kamen who is his favourite toon. He doesn't like to eat capsicum but her mother forces him to eat it or else she would beat him.
+              Shin chan's mother, Mitsi, is the main character after Shin Chan. She is a perfect housewife. She takes care of all the family members. However, she is little bad-tempered. She hits Shin Chan on his head which forms a bulging on his head. She hides some money which she calls her secret savings. She spends them on buying expensive clothes or cosmetics. Shin chan, by some means, always, comes to know where she has hidden her secret savings and then blackmails her that he would tell that to his dad. She, having no choice, buys him either Action kamen set or Choco chips.
+              He lives in Japan. He goes to a kindergarten school. He has many friends. His main friends are Kazama and Masao. Shin chan calls his principal as "Gang Leader" who has a very rough face like a real gang leader.  But he is a good natured and kind hearted person.
+              Shin chan has a pet dog named Shiro. Shin chan does not take good care of his dog. He never gives him his morning food. Though he is a naughty boy, everyone likes him very much.`,
+            },
+          },
+          {
+            label: 'Shin Chan - MCQ',
+            type: "mcq",
+            data: {
+              title: "Multiple Choice Question",
+              questions: [
+                {
+                  qText: `Shin Chan has a pet _______.`,
+                  options: `dog, cat, rabbit, parrot`,
+                },
+                {
+                  qText: `Shin Chan's mother is a _________.`,
+                  options: `housewife, teacher, principal, doctor`,
+                },
+                {
+                  qText: `Shin Chan's mother hits him on his _______.`,
+                  options: `head, nose, cheek, hand`,
+                },
+                {
+                  qText: `Shin Chan does not like ________.`,
+                  options: `capsicum, beetroot, carrot, brinjal`,
+                },
+                {
+                  qText: `Shin Chan lives in ___________.`,
+                  options: `Japan, China, America, India`,
+                },
+              ],
+            }
+          },
+          {
+            label: 'Dora',
+            type: "passage",
+            data: {
+              title: `Dora`,
+              img: "https://s3.ap-south-1.amazonaws.com/pschool.in/assets/english/dora.png",
+              text: `Dora is my favorite cartoon character. She is a very determined girl and always keep moving forward to reach her goal. 
+              Dora's adventures are usually about helping others who are in trouble. She has all the tools for her journey in her backbag. She knows how to read map to reach her destination. Boots is a monkey and it joins Dora in all her journey. It usually wears nothing but his beloved red boots, hence his name. 
+              Swiper is a fox. It wears a blue mask and gloves. It always tries to steal from Dora or others. But Dora doesn't punish the fox and simply shouts "NO'. The fox respects her and runs away.
+              Benny is another friend of Dora. He is a big, strong bull with a huge appetite and an even bigger heart. He often tries to help Dora on her adventures, but Benny is so accident-prone that it is usually him in need of Dora's help.`,
+            },
+          },
+          {
+            label: 'Dora - MCQ',
+            type: "mcq",
+            data: {
+              title: "Multiple Choice Question",
+              questions: [
+                {
+                  qText: `Who has a bad character?`,
+                  options: `Swiper, Boots, Benny, Isa`,
+                },
+                {
+                  qText: `Dora always carries a _____`,
+                  options: `backbag, suitcase, plastic bag, carry bay`,
+                },
+                {
+                  qText: `Dora uses ________ to reach her destination.`,
+                  options: `Map, Boots, Mobile, Backbag`,
+                },
+                {
+                  qText: `Dora's journey usually has ______ steps.`,
+                  options: `three, two, four, five`,
+                },
+                {
+                  qText: `Who is a bull?`,
+                  options: `Benny, Swiper, Boots, Isa`,
+                },
+              ],
+            }
+          },
+          {
+            label: 'Chhota Bheem',
+            type: "passage",
+            data: {
+              title: `Chhota Bheem`,
+              img: 'https://s3.ap-south-1.amazonaws.com/pschool.in/assets/english/chotobeem.webp',
+              text: `Once upon a time, there was a town named Dholakpur, ruled by a king, Indravarma. There lived a boy named Bheem. He was an intelligent, strong, and witty boy.  Bheem always fought for justice and destroyed evil.
+              Bheem was very populer in his town because he helped whoever was in need. Sometimes he even helped the king, and received lot of appreciation from the king and the people.
+              Bheem’s best friends were Raju, Jaggu and Chutki. Jaggu was a monkey and he was very active. Raju was a very naughty and cute boy. Chutki was a very cute and beautiful girl. 
+              Kalia was always jealous of Bheem, but Bheem liked him too.  Bheem was so good that he always helped Kalia whenever he fell in trouble, and even Kalia didn’t forget to help Bheem in return when needed. Dholu and Bholu, were identical twins and they were often seen with Kalia.
+              Bheem and his friends worked as a team to achieve success.`,
+            },
+          },
+          {
+            label: 'Chhota Bheem - MCQ',
+            type: "mcq",
+            data: {
+              title: "Multiple Choice Question",
+              questions: [
+                {
+                  qText: `Who is jealous of Bheem?`,
+                  options: `Kalia, Chutki, Dholu, Bholu`,
+                },
+                {
+                  qText: `Identical twins means, they _________`,
+                  options: `have same face and appearence, have same weight and height, are always naughty, are always helpful `,
+                },
+                {
+                  qText: `The king praises Bheem because Bheem ___________.`,
+                  options: `helps the king often, eats laddos nicely, is smart, is a good boy`,
+                },
+                {
+                  qText: `Bheem's favorite sweet is _______`,
+                  options: `Laddo, Jalebi , Barfi, Halwa`,
+                },
+                {
+                  qText: `Dholu and Bholu are very close to _________.`,
+                  options: `Kalia, Bheem, King, Chutki`,
+                },
+              ],
+            }
+          },
+          {
+            type: "sequence",
+            label: "Order Word (6 letter words)",
+            data: { 
+              text: `MOTHER, ADJUST, ADVICE, ACTION, FATHER, SIMPLE, GROUND, BRIDGE, DESIGN, SINGLE`
+             },
+          },
+          {
+            label: "Question Sentence",
+            type: 'sequence',
+            data: {
+                text : `what is your name
+                how old are you
+                can I help you
+                how are you feeling
+                what is the matter
+                what is your favorite snack
+                where is the restroom
+                can you help me
+                where is your mother
+                can you drive a car`
+            }
+        },
+        {
+          label: "Collective Nouns",
+          type: "matchByDragDrop",
+          data: {
+            isPractice: false,
+            title: "Drag and drop the collective nouns at proper places.",
+            styles: {
+              fontSize: "1rem",
+              dashWidth: 70,
+            },
+            text: `A ~herd~ of cattle
+            A ~flock~ of birds
+            A ~school~ of fish
+            A ~pack~ of wolves
+            A ~ swarm~ of bees
+            A ~choir~ of singers
+            A ~troupe~ of dancers`,
+          },
+        },
+          {
+            type: "rightOne",
+            label: "Pick Correct Spelling",
+            data: {
+              text: `class clas 
+              cold kold
+              memory memary 
+              butter buter
+              sugar suger
+              water watar
+              once onse
+              cattle catle
+              wheat wheet
+              meals meels
+              doctor doktor
+              bomb bom
+              ghost gost
+              potatoes potatos
+              listen lisen 
+              seed ceed
+              tomatoes tomatos
+              catch katch`
+            },
+          },
+              {
+                type: "fillupOptions",
+                label: "Article",
+                data: { type: "article" },
+              },
+              {
+                type: "fillupOptions",
+                label: "Choose Appropriate Word",
+                data: { type: "passage1" },
+              },
+              {
+                type: "matchByDragDrop",
+                label: "Match Antonyms",
+                data: { type: "antonyms", level: 2 },
+              },
+              {
+                type: "matchByDragDrop",
+                label: "Match Synonyms",
+                data: { type: "synonyms", level: 2 },
+              },
+              {
+                type: "matchByDragDrop",
+                label: "Match Antonyms - 2",
+                data: { type: "antonyms", level: 3 },
+              },
+              {
+                type: "matchByDragDrop",
+                label: "Match Synonyms - 2",
+                data: { type: "synonyms", level: 3 },
+              },
+        ]
