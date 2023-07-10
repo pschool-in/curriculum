59e4d3f3705518fc92815de22ee8cb75390c6aed
Rethna <rethna2@gmail.com>
2023-07-10 12:27:29 +0530

    added files

---
+++ languages/content/kannada/imgsInfo/picture-4.js
@@ -0,0 +1,112 @@
+export default {
+  id: "picture-4",
+  label: "House and Others",
+  list: [
+    {
+      id: "nature",
+      text: `man| Man
+woman | Woman
+boy|Boy
+girl | Girl
+baby | Baby
+house| House
+table|Table
+bed|Bed
+candle|Candle
+chair|Chair
+shelf|Shelf
+coat|Coat
+glasses|Glasses
+clock | Clock
+watch | Watch
+flower-pot | Flower Pot
+umbrella | Umbrella
+door | Door
+broom | Broom
+window | Window
+sofa | Sofa
+bucket | Bucket
+toilet | Toilet
+soap | Soap
+backpack | Backpack
+pillow | Pillow
+basket | Basket
+dustbin | Dustbin
+bed-lamp | Bed Lamp
+handbag | Handbag
+air-conditioner | Air Conditioner
+washing-machine | Washing Machine`,
+    },
+    {
+      id: "dress",
+      text: `dress | Dress
+coat | Coat
+shoe | Shoe
+sock | Sock
+boots | Boots
+pyjama | Pyjama
+gloves | Gloves
+scarf | Scarf
+sweater | Sweater
+bib | Bib
+shirt | Shirt
+jacket | Jacket
+hat | Hat
+trousers | Trousers`,
+    },
+    {
+      id: "school",
+      text: `pen |Pen
+pencil|Pencil
+ruler|Ruler
+eraser|Eraser
+scissors|Scissors
+sharpener|Sharpener
+calculator| Calculator
+book| Book
+ink | Ink
+schoolbag| Schoolbag
+waterbottle| Waterbottle
+color-pencils| Color Pencils
+crayons | Crayons`,
+    },
+    {
+      id: "kitchen",
+      text: `stove |Stove
+fork|Fork
+knife|Knife
+pan|Pan
+spoon|Spoon
+pot|Pot
+butter|Butter
+plate | Plate
+tap | Tap
+lid | Lid
+
+blender | Blender
+cheese | Cheese
+fridge | Fridge
+lollipop | Lollipop
+salad | Salad
+sugar | Sugar
+bowl | Bowl
+chicken | Chicken
+ice-cream | Ice cream
+meat | Meat
+
+salt | Salt
+sweets | Sweets
+bread | Bread
+cup | Cup
+juice | Juice
+mushroom | Mushroom
+sandwich | Sandwich
+cake | Cake
+flour | Flour
+lentils | Lentils
+
+rice | Rice
+soup | Soup`,
+    },
+  ],
+};
---
+++ languages/content/telugu/imgsInfo/picture-5.js
@@ -0,0 +1,70 @@
+export default {
+  id: "picture-5",
+  label: "More Animals and Birds",
+  list: [
+    {
+      id: "animals",
+      text: `mongoose| Mongoose
+            mosquito| Mosquito
+            octopus| Octopus
+            wolf| Wolf
+            lizard| Lizard
+            cockroach| Cockroach
+            garden-lizard| Garden Lizard
+rabbit| Rabbit
+leopard| Leopard
+dragonfly |Dragonfly
+fly|Housefly
+squirrel|Squirrel
+dinosaur | Dinosaur
+alligator | Alligator
+boar | Boar
+cricket | Cricket
+hyena | Hyena
+polarbear | Polar Bear
+antelope | Antelope
+bug | Bug
+jackal | Jackal
+prawn | Prawn
+baboon | Baboon
+python | Python
+bison | Bison
+chameleon | Chameleon
+porcupine | Porcupine
+tigerfish | Tigerfish
+ladybug | Ladybug
+seahorse | Seahorse
+hamster | Hamster
+mole | Mole
+caterpillar | Caterpillar
+earthworm | Earthworm
+hedgehog | Hedgehog
+stingray | Stingray
+jellyfish | Jellyfish
+starfish | Starfish`,
+    },
+    {
+      id: "birds",
+      text: `hornbill | Hornbill
+            cuckoo | Cuckoo
+            pheasant | Pheasant
+            falcon | Falcon
+            swan | Swan
+            bulbul | Bulbul
+            spotted-dove | Spotted Dove
+pelican | Pelican
+hummingbird | Hummingbird
+robin | Robin
+swallow | Swallow
+blue-jay | Blue jay
+seagull | Seagull
+canary | Canary
+goose | Goose
+turkey | Turkey
+hawk | Hawk
+cardinal | Cardinal
+parakeet | Parakeet
+macaw | Macaw`,
+    },
+  ],
+};
