52ea663cf372c023468b685d5a71230e2204d881
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-07-18 13:31:01 +0530

    created picture-5.js, picture-10.js

---
+++ languages/malayalam/imgsInfo/picture-10.js
@@ -0,0 +1,79 @@
+export default {
+  id: 'picture-10',
+  label: 'Social and Science',
+  list: [
+    {
+      id: 'social',
+      label: 'Social Science',
+      text: `mountain | മല|mala
+seashore | കടൽത്തീരം|kadaltheeram
+forest | വനം|vanam
+desert | മരുഭൂമി|marubhoomi
+river | നദി|nadi
+grassland | പുൽമേട്|pullmed
+ocean | സമുദ്രം|samudram
+polar | ധ്രുവം|dhruvam
+valley | താഴ്വര|thaazhvazaha
+volcano | അഗ്നിപർവ്വതം|agniparvatham
+apartment | Apartment
+bamboo-house | മുള വീട്|mula veedu
+hut | കുടിൽ|kudil
+igloo | Igloo
+tent-house | കൂടാരം |koodaram
+tree-house | Tree House
+road | റോഡ്|road
+railway |റയില്‍വഴി|rail vazhi
+well | കിണര്‍|kinar
+airport | വിമാനത്താവളം|vimanathavalam
+bank | ബാങ്ക്|baank
+classroom | ക്ലാസ് മുറി|class muri
+factory | പണിശാല|panishala
+fort |കോട്ട|kotta
+gopuram 
+harbour | തുറമുഖം|thuramukham
+hospital | ആശുപത്രി|aashupatri
+library | പുസ്തകശാല|pusthakashala
+lighthouse | വിളക്കുമാടം|vilakkumaadam
+map | ഭൂപടം |bhoopadam
+office | ഓഫീസ്|oofise
+palace | കൊട്ടാരം|kottaram
+parkinglot | Parking Lot
+restaurant | ഭക്ഷണശാല|bhakshanashaala
+stage | തിയേറ്റർ|theater
+supermarket | Super Market
+escalator | Escalator
+lift | Lift
+petrolbunk | Petrol Bunk
+footpath | കാൽനട പാത|kaal nadapaatha
+pillar | തൂണ്|thoon`
+    },
+    {
+      id: 'organs',
+      label: 'Organs',
+      text: `heart|ഹൃദയം|hridayam
+brain|തലച്ചോറ്|thalchor
+lungs|ശ്വാസകോശം|shwasakosham
+stomach|ആമാശയം|aamashayam
+liver|കരൾ|karal
+kidney|വൃക്ക|vrikha`
+    },
+    {
+      id: 'teeth',
+      label: 'Teeth',
+      text: `incisor | Incisor
+canine | Canine
+premolar | Premolar
+molar | അണപ്പല്ല്‌`
+    },
+    {
+      id: 'science',
+      label: 'Science',
+      text: `windmill | കാറ്റാടിയന്ത്രം
+solar-panel | Solar Panel
+bridge | പാലം
+dam | അണക്കെട്ട്
+water-tower | Water Tower
+electricpole | Electric Pole`
+    }
+  ]
+};
--- languages/malayalam/imgsInfo/picture-5.js
+++ languages/malayalam/imgsInfo/picture-5.js
@@ -5,67 +5,63 @@ export default {
     {
       id: 'action',
       label: 'Actions',
-      text: `drink| Drink
-        eat| Eat
-        bend | Bend
-        climb | Climb
-        jump | Jump
-        kneel | Kneel
-        pull | Pull
-        push | Push
-        sing | Sing
-        dance | Dance
-        running | Run
-        walking | Walk
-        sit | Sit
-        party | Party
-        skipping | Skip
-        catch | Catch
-        pass | Pass
-        swim | Swim
-        swing | Swing
-        throw | Throw
-        carry | Carry
-        lifting | Lift
-        mopping | Moping
-        riding | Ride
-        sweep | Sweep
-        cough | Cough
-        sick | Sick
-        sneeze | Sneeze
-        yawn | Yawn
-        sleep | Sleep`
+      text: `drink| കുടിക്കുക|kudikukka
+eat| കഴിക്കുക|kazhikukka
+bend | വളയുക|valayuka
+climb | കയറുക|kyaruka
+jump | ചാടുക|chaduka
+kneel | മുട്ടുകുത്തുക|muttukuthuka
+pull | വലിക്കുക |valikuka
+push | തള്ളുക|thalluka
+sing | പാടുക|paaduka
+dance | ന്യത്തം ചെയ്യുക|nirtham cheyyuka
+running | ഓടുക|ooduka
+walking | നടക്കുക|nadakkuka
+sit | ഇരിക്കുക|izhikuka
+party | Party
+skipping | Skip
+catch | പിടിക്കുക|pidikuka
+pass | Pass
+swim | നീന്തുക|neethuka
+swing | ഊഞ്ഞാലാടുക|oonjaladuka
+throw | എറിയുക|eriyuka
+carry | എടുത്തുകൊണ്ടു പോകുക|eduthukond pokuka
+lifting | പൊക്കുക|pokkuka
+mopping | Moping
+riding | സവാരിചെയ്യുക|savari cheyyuka
+sweep | തൂത്തുവാരുക|thootuvaazhuka
+cough | ചുമയ്ക്കുക|chumaykuka
+sick | Sick
+sneeze | തുമ്മുക|thummuka
+yawn | കോട്ടുവായിടുക|kotvayiduka
+sleep | ഉറങ്ങുക|uranguka`
     },
     {
       id: 'sports',
       label: 'Sports',
-      text: `bat2 | Bat
-        racket | Racket
-        seesaw | Seesaw
-        slide | Slide
-        carrom | Carrom
-        chess | Chess
-        cricket2 | Cricket
-        tennis | Tennis
-        football | Football
-        hockey | Hockey
-        kabaddi | Kabaddi
-        archery | Archery
-        basketball | Basket ball
-        boxing | Boxing
-        table-tennis | table-tennis
-        golf | Golf
-        wrestling | Wrestling
-        
-        
-        karate | Karate
-        surfing | Surfing
-        
-        jockey | Jockey
-        track | Track
-        
-        bandage | Bandage
-        danger | danger`
+      text: `bat2 | ബാറ്റ്‌|baat
+racket | Racket
+seesaw | Seesaw
+slide | Slide
+carrom | Carrom
+chess | ചതുരംഗം|chathurangam
+cricket2 | ക്രിക്കറ്റുകളി|cricket kali
+tennis | ടെന്നീസ്‌ കളി|tennis kali
+football | കാല്‍പ്പന്തുകളി|kaalpanthkali
+hockey | ഹോക്കി|hockey
+kabaddi | കബഡി|kabadi
+archery | അമ്പെയ്ത്ത്|ambeyth
+basketball | Basket ball
+boxing | Boxing
+table-tennis | table-tennis
+golf | Golf
+wrestling | ഗുസ്തി |gusthi
+karate | Karate
+surfing | Surfing
+jockey | Jockey
+track | Track
+bandage | Bandage
+danger | ആപത്ത്‌|aapath`
     }
   ]
 };
