4b5881a98d412b5a1484684c809da6c9ead312c9
priya2786 <priyahh123@gmail.com>
2023-07-20 14:44:35 +0530

    20-7

--- languages/telugu/imgsInfo/picture-10.js
+++ languages/telugu/imgsInfo/picture-10.js
@@ -5,75 +5,75 @@ export default {
       {
         id: 'social',
         label: 'Social Science',
-        text: `mountain | Mountain
-        seashore | Seashore
-        forest | Forest
-        desert | Desert
-        river | River
-        grassland | Grassland
-        ocean | Ocean
-        polar | Polar
-        valley | Valley
-        volcano | Volcano
-        apartment | Apartment
-          bamboo-house | Bamboo House
-          hut | Hut
-          igloo | Igloo
-          tent-house | Tent House
-          tree-house | Tree House
-          road | Road
-          railway | Railway
-          well | Well
-          airport | Airport
-          bank | Bank
-          classroom | Class Room
-          factory | Factory
-          fort | Fort
-          gopuram 
-          harbour | Harbour
-          hospital | Hospital
-          library | Library
-          lighthouse | Light House
-          map | Map
-          office | Office
-          palace | Palace
-          parkinglot | Parking Lot
-          restaurant | Restaurant
-          stage | Theatre
-          supermarket | Super Market
-          escalator | Escalator
+        text: `mountain | పర్వతం|Parvatam
+        seashore | సముద్ర తీరం|Samudra tiram
+        forest | అడవి|Adavi
+        desert | ఎడారి|Edari
+        river | నది|Nadi
+        grassland | గడ్డి భూములు|Gaddi bhumulu
+        ocean | సముద్ర|Samudra
+        polar | ధ్రువ|Dhruva
+        valley | లోయ|Loya
+        volcano | అగ్నిపర్వతం|Agniparvatam
+        apartment | అపార్ట్‌మెంట్|Apartment
+          bamboo-house | వెదురు ఇల్లు|Veduru illu
+          hut | గుడిసె|Gudise
+          igloo | ఇగ్లూ|Igloo
+          tent-house | టెంట్ హౌస్|Tent House
+          tree-house | చెట్టు మీద కట్టుకున్న ఇల్లు|Cettu mida kattukunna illu
+          road | త్రోవ|Trova
+          railway | రైల్వే|Railway
+          well | బాగా|Baga
+          airport | విమానాశ్రయం|Vimanasrayam
+          bank | బ్యాంకు|Bank
+          classroom | తరగతి గది|Taragati gadi
+          factory | ఫ్యాక్టరీ|Factory
+          fort | కోట|Kota
+          gopuram | గోపురం|gopuram
+          harbour | నౌకాశ్రయం|Naukasrayam
+          hospital | ఆసుపత్రి|asupatri
+          library | గ్రంధాలయం|Grandhalayam
+          lighthouse | లైట్ హౌస్|Light House
+          map | మ్యాప్|Map
+          office | కార్యాలయం|Karyalayam
+          palace | రాజభవనం|Rajabhavanam
+          parkinglot | వాహనములు నిలుపు స్థలం|Vahanamulu nilupu sthalaṁ
+          restaurant | రెస్టారెంట్|Restaurant
+          stage | థియేటర్|Theatre
+          supermarket | సూపర్ మార్కెట్|Super Market
+          escalator | ఎస్కలేటర్|Escalator
           lift | Lift
-          petrolbunk | Petrol Bunk
-          footpath | Foot Path
-          pillar | Pillar`
+          petrolbunk | పెట్రోల్ బంక్|Petrol Bunk
+          footpath | ఫుట్ పాత్|Foot Path
+          pillar | పిల్లర్|Pillar`
       },
       {
         id: 'organs',
         label: 'Organs',
-        text: `heart
-        brain
-        lungs
-        stomach
-        liver
-        kidney`
+        text: `heart|గుండె|Gunde
+        brain|మె ద డు|Me da du
+        lungs|ఊపిరితిత్తులు|upiritittulu
+        stomach|కడుపు|Kadupu
+        liver|కాలేయం|Kaleyam
+        kidney|మూత్రపిండము|Mutrapindamu`
       },
       {
         id: 'teeth',
         label: 'Teeth',
-        text: `incisor | Incisor
-        canine | Canine
-        premolar | Premolar
-        molar | Molar`
+        text: `incisor | కోత|Kota
+        canine | కుక్కల|Kukkala
+        premolar | ప్రీమోలార్|Premolar
+        molar | మోలార్|Molar`
       },
       {
         id: 'science',
         label: 'Science',
-        text: `windmill | Windmill
-        solar-panel | Solar Panel
-        bridge | Bridge
-        dam | Dam
-        water-tower | Water Tower
-        electricpole | Electric Pole`
+        text: `windmill | విండ్మిల్|Windmill
+        solar-panel | సోలార్ ప్యానల్|Solar Panel
+        bridge | వంతెన|Vantena
+        dam | ఆనకట్ట|anakatta
+        water-tower | నీటి స్థంభం|Niti sthambham
+        electricpole |ఎలక్ట్రిక్ పోల్| Electric Pole`
       }
     ]
   };
