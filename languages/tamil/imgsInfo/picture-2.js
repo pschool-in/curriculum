5ceba7ae487256e2251e645dbc6125772f40c028
priya2786 <priyahh123@gmail.com>
2023-08-04 17:00:19 +0530

    aug 4th update

--- languages/tamil/imgsInfo/picture-2.js
+++ languages/tamil/imgsInfo/picture-2.js
@@ -8,60 +8,80 @@ export default {
       text: `apple | ஆப்பிள்  
 grapes | திராட்சை
 banana | வாழைப்பழம்
-cherry | Cherry
-avocado | Avocado
-strawberry | strawberry
+cherry | 
+avocado | 
+strawberry | 
 guava | கொய்யாப்பழம்
 jackfruit | பலாப்பழம்
 mango | மாம்பழம்
 orange | ஆரஞ்சு
 
 papaya |பப்பாளி
-pineapple | அன்னாசிப்பழம்
-watermelon |  தர்பூசணி
-pomegranate| மாதுளை
-fig| அத்திப்பழம்
-custard-apple| சீதாப்பழம்
-plum| 	plum
-dates| பேரிச்சம்பழம்
-muskmelon| முலாம்பழம்`
+pineapple |அன்னாசிப்பழம்
+watermelon |தர்பூசணி
+pomegranate|மாதுளை
+fig|அத்திப்பழம்
+custard-apple|சீதாப்பழம்
+plum|plum
+dates|பேரிச்சம்பழம்
+muskmelon|முலாம்பழம்
+Lemon|எலுமிச்சை`
     },
     {
       id: 'vegetables',
       label: 'காய்கறிகள்',
-      text: `carrot | 	காரட்
-beetroot | பீட்ரூட்
-onion | வெங்காயம்
-tomato |  தக்காளி
-potato | உருளைக்கிழங்கு
-cabbage | முட்டைகோசு
-corn | மக்காச்சோளம்
-cucumber | வெள்ளரிக்காய்
-garlic|பூண்டு
-ginger|இஞ்சி
-
+      text: `carrot |காரட்
+beetroot |பீட்ரூட்
+onion|வெங்காயம்
+tomato|தக்காளி
+potato|உருளைக்கிழங்கு
+cabbage|முட்டைகோசு
 cauliflower|காலிஃபிளவர்
-pumpkin| பூசணிக்காய்
-almond| பாதாம்
-beans| பீன்ஸ்
-bitter-gourd|	பாகற்காய்
-brinjal| 	கத்திரி
-capsicum| குடைமிளகாய்
-cashewnut| முந்திரி
-chilli|பச்சைமிளகாய்
-coconut | தேங்காய்
-Lemon|எலுமிச்சை
+corn|மக்காச்சோளம்
+cucumber|வெள்ளரிக்காய்
+garlic|பூண்டு
 
-corianderleaf | கொத்தமல்லி
-curryleaf | கருவேப்பிலை
-drumstick| முருங்கைக்காய்
+ginger|இஞ்சி
+pumpkin|பூசணிக்காய்
+beans|பீன்ஸ்
+bitter-gourd|பாகற்காய்
+brinjal|கத்திரி
+capsicum|குடைமிளகாய்
+chilli|பச்சைமிளகாய்
+coconut|தேங்காய்
+corianderleaf| கொத்தமல்லி
+curryleaf|கருவேப்பிலை
+mushroom | காளான்
+
+drumstick|முருங்கைக்காய்
 greenpeas|பச்சை பட்டாணி
-ladies-finger |  வெண்டைக்காய்
-mintleaf| புதினா
-peanut| வேர்க்கடலை
-radish| முள்ளங்கி
-spinach | கீரை
-vegetables |காய்கறிகள்`
+ladies-finger| வெண்டைக்காய்
+mintleaf|புதினா
+peanut|வேர்க்கடலை
+radish|முள்ளங்கி
+spinach|கீரை
+vegetables|காய்கறிகள்
+almond|பாதாம்
+cashewnut|முந்திரி`
+    },
+    {
+      id: 'cooking',
+    label: 'சமையல் பொருட்கள்',
+    text:
+         `lentils|பருப்பு
+         rice|அரிசி
+         kadalai-paruppu|கடலைபருப்பு
+         malli|கொத்தமல்லி விதை
+         pepper|மிளகு
+         mochai| மொச்சை
+         moong-dhal|பாசிப்பருப்பு
+         pattani|பட்டாணி
+         seeragam|சீரகம்
+         sombu|சோம்பு
+         toor-dhal|துவரம் பருப்பு
+         urad-dhal| உளுத்தம் பருப்பு
+         wheat|கோதுமை
+         paddy|நெல்`
     }
   ]
 };
