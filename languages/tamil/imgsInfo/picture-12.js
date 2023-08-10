8e025e919b50acd54eca24a56d2717b44abf086b
priya2786 <priyahh123@gmail.com>
2023-08-10 17:20:31 +0530

    pic-12 update

---
+++ languages/tamil/imgsInfo/picture-12.js
@@ -0,0 +1,71 @@
+export default {
+    id: 'picture-12',
+    label: 'எழுத்தும் படமும்',
+    list: [
+      {
+        id: 'vowels',
+        label: 'உயிர் எழுத்துக்கள் ',
+        text: `
+        mother|அம்மா
+        goat|ஆடு
+        leaf|இலை
+        fly|ஈ
+        world|உலகம்
+        whistle|ஊதல்
+        rat| எலி
+        ladder| ஏணி
+        five|ஐந்து
+        camel| ஒட்டகம்
+        rooftile|ஓடு
+        auvai|ஔவை`
+      },
+      {
+        id: 'consonants',
+        label: 'மெய் எழுத்துக்கள்',
+        text: 
+        `wheel|சக்கரம்
+        conch|சங்கு
+        stick|குச்சி
+         ginger|இஞ்சி
+         shirt | சட்டை
+         eye |கண்
+         knife|கத்தி
+         ball | பந்து
+         hat | தொப்பி
+         tree | மரம்
+         mat | பாய்
+         rishi|முனிவர்
+         stone|கல்
+         வ்|
+         yazl| யாழ்
+         cucumber|வெள்ளரிக்காய்
+         chair|நாற்காலி
+         fish|மீன்`
+      },
+      {
+        id: 'consonants1',
+        label: 'உயிர் மெய் எழுத்துக்கள்',
+        text: 
+        `
+        donkey|  கழுதை
+         ங|
+         salangai|சலங்கை
+         ஞ|
+         kite|பட்டம்
+         bowl|கிண்ணம்
+         frog| தவளை
+         fox| நரி
+         pig|பன்றி
+         bell|மணி
+         potter|குயவர்
+         king|அரசன்
+         ladoo|லட்டு
+         net |வலை
+         fruits|பழங்கள்
+         corn|சோளம்
+         bird|பறவை 
+         rainbow|வானவில்`
+      }
+    ]
+  };
+  
