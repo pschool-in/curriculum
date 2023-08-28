48a36f048105c23aa5c7fc3ea3a40f12fb7d3fb7
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-08-28 12:42:55 +0530

    created

---
+++ languages/marathi/imgsInfo/basic.js
@@ -0,0 +1,65 @@
+export default {
+  id: 'basic',
+  label: 'साधा शब्द',
+  list: [
+    {
+      id: 'vowels',
+      label: 'साधे शब्द - स्वर|Simple Words - Vowels',
+      text: `bear|अस्वल	
+gooseberry|आवळा	
+apartment|इमारत	
+lemon|ईडलिंबू	
+pillow|उशी	
+sugarcane|ऊस	
+rishi|ऋषी	
+|एक	
+|ऐरण	
+mouth|ओठ	
+medicine|औषध	
+fig|अंजीर	
+|स्वतः	
+bat2|बॅट 	
+Octopus|ऑक्टोपस`
+    },
+    {
+      id: 'consonants',
+      label: 'साधे शब्द - व्यंजन|Simple Words - Consonants',
+      text: `lotus|कमळ	
+chalk|खडू	
+cluster-beans|गवार	
+house|घर	
+|वाङमय	
+glasses|चष्मा	
+umbrella|छत्री	
+ship|जहाज	
+dress|झगा	
+|झान्झ	
+watermelon|टरबूज	
+|ठसा	
+damaru|डमरू	
+cloud|ढग	
+arrow|बाण 	
+tabla|तबला	
+|थवा	
+stone|दगड	
+bow|धनुष्य	
+nails|नखे	
+kite|पतंग	
+blackboard|फळा	
+almond|बदाम	
+|भटजी	
+crocodile|मगर	
+liver|यकृत	
+chariot|रथ	
+garlic|लसूण	
+forest|वन	
+ostrich|शहामृग	
+hexagon|षट्कोन	
+shirt|सदरा	
+chin|हनुवटी	
+baby|बाळ	
+warroir|क्षत्रिय	
+|ज्ञानेश्वर	`
+    }
+  ]
+};
