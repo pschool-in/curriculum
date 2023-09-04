bc62a59b9557a40a3ce4ef871eea1045af0be694
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-09-04 15:25:46 +0530

    updated

---
+++ languages/marathi/catalog.js
@@ -0,0 +1,186 @@
+export default {
+  label: 'Malayalam Catalog',
+  list: [
+    {
+      label: 'Letter (अक्षरे)',
+      id: 'mr-letters',
+      text: `Tracing activity of vowels accompanied by audio
+        Tracing activity of consonants accompanied by audio 
+        Listen to audio and identify Vowels
+        Listen to audio and Identify Consonants
+        Listening Activity on based on consonants and matras
+        Listen to audio and identify letter
+        match activity based on combination of vowels and consonants
+        Listening Activity on Vowels
+        Listening Activity on Consonants
+        Listen and touch activity on Vowels
+        Listen and touch activity on Consonants`
+    },
+    {
+      label: 'Words (शब्द)',
+      id: 'mr-word',
+      text: `Word forming activity (2 Letter Words)
+        Word forming activity (3 Letter Words)
+        Word forming activity (4 Letter Words)
+        Form Words activity
+        Match up activity on Synonyms
+        Match up activity on Antonyms
+        Mcq on Synonyms
+        Mcq Antonyms
+        Complete the word by reading the hint`
+    },
+    {
+      label: 'Words (शब्द - 2)',
+      id: 'mr-word-2',
+      text: `Word forming activity (2 Letter Words)
+        Word forming activity (3 Letter Words)
+        Match up activity on Synonyms
+        Mcq on Synonyms
+        Match up activity on Antonyms
+        Reading passage on Compound Words
+        Match up activity on Compound Words
+        Fill up activity on compound words`
+    },
+    {
+      label: 'Word Practice (शब्दांचा सराव)',
+      id: 'mr-word-practice',
+      text: `Word forming activity (2 Letter Words)  – Practice
+        Word forming activity (2 Letter Words) – More Practice
+        Word forming activity (3 Letter Words) – Practice
+        Word forming activity (3 Letter Words) – More Practice
+        Match up on activity on Synonyms
+        Match up activity on Antonyms`
+    },
+    {
+      label: 'Colors (रंग)',
+      id: 'mr-colors',
+      text: `Match Colors with names
+        Connect letters and form the color names
+        Identify the colors in a rainbow`
+    },
+    {
+      label: 'Numbers (अंक)',
+      id: 'mr-numbers',
+      text: `Match up activity on Numbers
+        Connect letters to find the number names
+        Arrange number names in ascending order
+        Match activity numerals and number names (more practice)
+        Arrange number names in ascending order (more practice)
+        Find the numeral from the number name`
+    },
+    {
+      label: 'Sentences (वाक्य)',
+      id: ' mr-sentence',
+      text: `Match words to form sentence activity 
+        Connect words to form sentence activity
+        Translate English sentences to Marathi activity`
+    },
+    {
+      label: 'Riddles (कोडी)',
+      id: ' mr-riddle',
+      text: `Riddles
+        Fill up activity on riddles`
+    },
+    {
+      label: 'Proverbs (म्हणी)',
+      id: 'mr-proverb',
+      text: `Reading passage on proverbs
+        Match up activity on proverbs
+        Fill up activity on proverbs
+        Mcq on Meaning of Proverbs`
+    },
+    {
+      label: 'Grammar (व्याकरण)',
+      id: 'mr-grammar',
+      text: `Reading passage on nouns
+        Match up activity on nouns
+        Identify Nouns in the given sentences
+        Classify the Nouns
+        Fill up activity on nouns
+        Mcq on types of nouns
+        Complete the Story based on nouns
+        Reading passage on pronouns
+        Match up activity on pronouns
+        Identify Pronouns in the given sentences
+        Fill up activity on pronouns
+        Mcq on types of pronouns
+        Complete the Story
+        Reading passage on adjectives
+        Match up activity on adjectives
+        Identify Adjectives in the given sentences
+        Identify whether Adjective or Not
+        Fill up activity on adjectives
+        Mcq on types of Adjectives
+        Complete the Story based on adjectives `
+    },
+    {
+      label: 'Grammar (व्याकरण – 2)',
+      id: 'mr-grammar-2',
+      text: `Reading passage on gender nouns
+        Match activity based on genders
+        Identify word that denotes a Gender
+        Classify if the following words belong to Feminine Gender
+        Classify if the following words belong to Masculine Gender
+        Classify if the following words belong to Neuter Gender
+        Fill up based on Genders
+        Complete the Story Based on Genders
+        Reading passage on singular and plural
+        Match activity based on singular and plural
+        Classify if the words are singular
+        Classify if the words are plural
+        Fill up activity based on singular and plural nouns
+        Mcq on Singular or Plural
+        Match activity on singular and plural
+        Complete the Story based on singular and plural`
+    },
+    {
+      label: 'Grammar (व्याकरण – 3)',
+      id: 'mr-grammar-3',
+      text: `Mcq on one word substitutions
+        Reading passage on Words with Multiple Meanings
+        Mcq on Words with Multiple Meaning
+        Match up activity on Words with Multiple Meaning`
+    },
+    {
+      label: 'Tenses (काळ)',
+      id: 'mr-tense',
+      text: `Reading passage on tenses 
+        Match up activity on tenses 
+        Identify Words Indicating Tense
+        Fill up activity on tenses 
+        Mcq on the Types of Tenses
+        Complete the Story based on tenses `
+    },
+    {
+      label: 'Poems (कविता)',
+      id: ' mr-poem ',
+      text: `10 Marathi Poems `
+    },
+    {
+      label: ' Story (कथा)',
+      id: 'mr-reading',
+      text: `20 Marathi Fables`
+    },
+    {
+      label: 'Science (विज्ञान)',
+      id: 'mr-science',
+      text: `Drag and drop activity on Parts of face, Parts of a Plant, Dog Parts, Parts of a Human Being, Parts of a Tree, The Organs, The Rainbow`
+    },
+    {
+      label: ' Social (सामाजिक विज्ञान)',
+      id: 'mr-social ',
+      text: `Drag and drop activity on Big Rivers, Cities, Neighbouring States, Directions, Landforms, People and Their Workplaces, People and Their Tools, States of South India, Rivers, Languages, Map - Cities
+        Listening Activity - The Story of the Mango Tree, Healthy Eating Habits, What We Do, Thirsty Crow`
+    },
+    {
+      label: 'Wordsearch (शब्द शोध)',
+      id: 'mr-wordsearch',
+      text: `10 Marathi wordsearch`
+    },
+    {
+      label: 'Crossword (शब्दकोडे)',
+      id: ' mr-crossword',
+      text: `5 Marathi Crosswords`
+    }
+  ]
+};
