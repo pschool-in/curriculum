24ab1ae55e8945f96d036e2b4cf76d9568f83ba1
Rethna <rethna2@gmail.com>
2023-09-05 20:09:40 +0530

    English basic

--- languages/english/imgsInfo/basic.js
+++ languages/english/imgsInfo/basic.js
@@ -1,9 +1,9 @@
 export default {
-  id: "simple",
+  id: 'simple',
   list: [
     {
-      id: "simple",
-      label: "Simple Words",
+      id: 'simple',
+      label: 'Simple Words',
       text: `ant| Ant
 bat2| Bat
 cat| Cat
@@ -23,7 +23,31 @@ pen| Pen
 quill| Quill
 rat| Rat
 sun| Sun
-tap| Tap`,
+tap| Tap`
     },
-  ],
+    {
+      id: 'simple',
+      label: 'Simple Words',
+      text: `apple| Apple
+ball| Ball
+clock| Clock
+dog| Dog
+eat| Eat
+fan| Fan
+gun| Gun
+hat| Hat
+ink| Ink
+jug| Jug
+key| Key
+log| Log
+mat| Mat
+nib| Nib
+oar| Oar
+pen| Pen
+quill| Quill
+rat| Rat
+sun| Sun
+tap| Tap`
+    }
+  ]
 };
