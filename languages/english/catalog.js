ee1c38f8055ad73827a8b6e9f6371573777e67c2
sanhashahal <89006046+sanhashahal@users.noreply.github.com>
2023-09-26 12:03:25 +0530

    updated

---
+++ languages/english/catalog.js
@@ -0,0 +1,154 @@
+export default {
+  label: 'Reading Catalog',
+  lang: 'en',
+  list: [
+    {
+      label: "Let's Speak",
+      id: 'conversation-3',
+      text: `Ranger Rick - Two friends lost in the forest and a Ranger helps them.
+        Day out with Mommy - Mother and daughter planning their day out.
+        Superheroes - Two friends roleplaying superheroes.
+        Game Time - Two friends about a newly released game.
+        The Monkey that went to School - A comical conversation between two friends about a monkey going to the school.
+        Monkey Circus - A comical conversation between friends planning to start a monkey circus.
+        Superpowers - A group of friends role-playing animals and superheroes.
+        Bobby - Two friends about their silly friend Bobby.
+        Dream -  A conversation between a mother and son.
+        Memories - A group of friends remembering their old mischiefs.
+        Chores - Mother and daughter discussing chores.
+        Rules - Mother and son discussing why rules are important to maintain a healthy routine.
+        Story Time - A story time between Grandma and Granddaughter.
+        Wrinkles - The curiosity of a grandchild about Granpa's wrinkles.
+        Lost -  A lost child trying to find help.`
+    },
+    {
+      label: "Let's Speak",
+      id: 'conversation-4',
+      text: `Grammar Class - Teacher teaching students grammar.
+        Career -  A father ans son discussing career options.
+        Ticket Reservation - A passenger enguiring seat availabilty of train and booking tickets.
+        At the Gift Shop - A customer shopping for a gift for her friend.
+        Educational Loan - A discussion of a student with the Bank Manger on different educational loans.
+        Train Ride - A conversation between a passenger and the conductor on the train.
+        At the Hospital - A doctor addressing a patients concerns.
+        At the Principal's Office - Parents and Principal discussing the students academic performance.
+        Cat and Dog - An imaginary conversation between a cat and a dog.
+        Coach - A conversation between a player and a coach.
+        Dream - A daughter explaining her dream to her mother.
+        Importance of Sports - Two friends discussing the importance of sports.
+        A Day at the Park -  A group of friends enjoying their picnic.
+        Parent-Teacher Meeting - A teacher and parent discussing a students academic performance.
+        Meeting an Old Friend - Two friends catching up after a long time.`
+    },
+    {
+      label: "Let's Speak",
+      id: 'conversation-5',
+      text: `Hobbies - Teacher and students discussing hobbies.
+        Vocabulary based activities based on the conversation.(mcq, fill up, riddles)
+        Catching Up - Two friend discussing their day at school.
+        Vocabulary based activities based on the conversation.(mcq, fill up, match activity on synonyms)
+        Friends Forever - Two friend discussing their plans for the school talent show and supporting each other.
+        Vocabulary based activities based on the conversation.(mcq, fill up, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)
+        A Day at School - A child discussing how her day went with her parents.
+        Vocabulary based activities based on the conversation.(mcq, fill up, match activity on synonyms)
+        Grandma's Interview - A child interviewing her Grandmother as part of her assignment.
+        Vocabulary based activities based on the conversation.(mcq, fill up, match activity on synonyms)`
+    },
+    {
+      label: "Let's Speak",
+      id: 'conversation-6',
+      text: `Birthday Party - A goup of friend planning a surprise birthday party for their friend.
+        At the Restaurant - A conversation between a waiter and a customer at a restaurent.
+        Group Project - Students discussing their group project.
+        Resolving a Conflict - Two friends resolving a conflit between them.
+        Trip to Hawaii -  Two friend planning a family trip together.
+        25th Anniversary -  Sibling palnning their parents anniversary party.
+        Halloween - Friends discussing about hallloween.
+        Holi - Friends dicussing about Holi.
+        Shoe Store -  A conversation between a customer and shopkeeper at a shoe store.
+        Princess - An imaginary conversation between fairytale princesses.
+        Ugly Duckling and the Swan -  A conversation based on thhe tale of The Ugly Duckling.
+        Avengers - A conversation between the Avengers.
+        Trip to the Beach - A group of friend enjoying their trip to the beach.
+        At the Police Station - A victim filing an FIR at the police station.
+        Dance Class - A conversation between teacher and students at the dance class.`
+    },
+    {
+      label: "Let's Speak",
+      id: 'conversation-7',
+      text: `Hobbies - Frinds discussing their newly found hobbies.
+        Curfew - A son asking his parents to extent his curfew.
+        Job Interview - An applicant attending the job interview.
+        Business Partners - A conversation between two business partners.
+        Assignment - A teacher giving her student a feedback on the assignment submitted by her.
+        Strangers - A conversation between to strangers on a public transport.
+        Debate - A debate between two election candidates.
+        Interview - An artist being interviewed by a journalist.
+        Smartphone - A customer enquiring about latest models of smartphones.
+        Food Fair - Students discussion on organizing a Food Fair at their school.
+        Favorite Artist - Friends discussing their favourite Artists and their trending tracks.
+        Game Challenge - A group of friends organizing a game challenge.
+        Water Park - Friends planning to visit a water park that opened in the town recently.
+        Choices - Two friends discussing about a job offer.`
+    },
+    {
+      label: 'Fiction',
+      id: 'fiction-4',
+      text: `The Dolphin Show - An excited kids visiting a Dolphin Village.
+        Activities based on the short story (mcq, arrange the events in order, true or false, fill up, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)
+        Peter's Swimming Lesson - A little tortoise overcoming its fears and learning to swim alone without anyones help.
+        Activities based on the short story (mcq, fill up, glossary, complete word activity based on glossary, match activity on synonyms and antonyms) 
+        The Lost Puppy - A young boy's determination to find a lost puppy's owner.
+        Activities based on the short story (mcq, glossary, complete word activity based on glossary, match activity on synonyms and antonyms) 
+        Shadow - A brave wolf rescuing a lost young wolf from a blizzard.
+        Activities based on the short story (mcq, glossary, complete word activity based on glossary, match activity on synonyms and antonyms) 
+        Aurora - A story of bravery and kindness of a fierce Polar Bear.
+        Activities based on the short story (mcq, fill up, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)`
+    },
+    {
+      label: 'Fiction-2',
+      id: 'fiction-4-2',
+      text: `The Adventures of a Lunchbox -  The life cycle of a lunch box from its making to recycling.
+        Activities based on the short story (mcq, arrange the events in order, true or false, fill up, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)
+        Topsy-Turvy Triumph - The story of a baker who refused to give up in the face of adversity.
+        Activities based on the short story (mcq, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)
+        A Journey of Discovery - A change in perspective unveils a new world of possibilities.
+        Activities based on the short story (mcq, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)
+        The Weather Report - Forecasts are predictions based on scientific data, but they can sometimes be wrong.
+        Activities based on the short story (mcq, fact vs opinion, fill up, riddles)
+        The Great Icecream Debate - Comparisson of two stores and understanding the facts and opinions.
+        Activities based on the short story (mcq, fact vs opinion, fill up, match activity)`
+    },
+    {
+      label: 'Fiction',
+      id: 'fiction-6',
+      text: `The Mystery of the Missing Lunchbox - A group of students and their teacher trying to find a missing lunchbox.
+        Activities based on the short story (mcq, fact vs opinion, fill up, riddles, match activity baesd on synonyms and antonyms)
+        The Butterfly Effect - A kind girl helps an injured butterfly.
+        Activities based on the short story (mcq, cause vs effect, fill up, match activity baesd on synonyms)
+        The Lost Key - A treasure hunt that began when a boy found a lost key.
+        Activities based on the short story (mcq, cause vs effect, fill up, riddles, match activity baesd on synonyms)
+        The Frog and The Mouse - A frog who tried to drown a foolish mouse ended up being a fish's dinner.
+        Activities based on the short story (mcq, fill up, glossary, complete word activity based on glossary, match activity on synonyms and antonyms)
+        The Eagle and The Jackdaw - Folktale of the foolish jackdaw who thought he was mightier than the eagle.
+        Activities based on the short story (mcq, fill up, glossary, complete word activity based on glossary)`
+    },
+    {
+      label: 'Kids Favourite Cartoons',
+      id: 'kids-favourite-cartoons',
+      text: `Passages on Shin Chan, Dora and Chhota Beem followed by mcqs based on the passage.`
+    },
+    {
+      label: 'Reading Passages',
+      id: 'passages',
+      text: ` Pure thoughts - A passage on purity.
+        An Interpretation - An nterpretation of the familiar doll of three wise monkeys.
+        A short passage on Spinner dolphins .
+        A short passage on Flowers.
+        Pranny and the wild horse - A kid's school adventure.
+        Tiny, the cute lizard - The lizard who refused to move out of her comfort zone.
+        Millie, an alien - Mini's encounter with an alien.
+        Neemoo, the little ant - A little ant's adventure.`
+    }
+  ]
+};
